e819c92e912137d9119f69b41e93143495868b0b
Marconi Poveda <marconipoveda84@gmail.com>
2023-06-11 17:33:53 -0600

    Inital Commit + First scripts
    Signed-off-by: Marconi Poveda <marconipoveda84@gmail.com>

---
+++ write-first/HelloWorld.ps1
@@ -0,0 +1,4 @@
+#Write-Output 'Hello World!'
+
+$name = Read-Host -Prompt "Please enter your Name"
+Write-Output "Congratulations $name! You have written a dumb script in PowerShell!"
---
+++ write-first/show-date.ps1
@@ -0,0 +1,5 @@
+$date = Get-Date -Format dd/MM/yy
+$name = Read-Host -Prompt "Enter your name!"
+
+Write-Output "Today's date is $date"
+Write-Output "Today is the day $name began learning Powershell!"
